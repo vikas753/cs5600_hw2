8b1bb396cbc93aa434024d210fc10836fced26c3
vikas753 <vvikas1996@gmail.com>
2020-01-15 00:23:15 -0500

    Update the github with working version of fibonacci assembly codes for all archs

--- fib.c
+++ fib.c
@@ -1,12 +1,41 @@
+////////////////////////////////////
+//
+// Fibonacci of a number 
+//
+////////////////////////////////////
 
 #include <stdio.h>
 #include <stdlib.h>
+
+long fib(long arg)
+{
+  if(arg == 0)
+  { 
+    return 0;
+  }	  
+  else if(arg == 1)
+  {
+    return 1;
+  }
+  else
+  {
+    return fib(arg-1) + fib(arg-2);	  
+  }
+}
 
 int
 main(int argc, char* argv[])
 {
     long xx = atol(argv[1]);
-    printf("fib(3) = %ld\n", xx);
+    if(xx < 0)
+    {
+      printf("Usage: ./fib N , where N >= 0 \n");	    
+    }
+    else
+    {    
+      long fibResult = fib(xx);
+      printf("fib(%ld) = %ld\n", xx , fibResult);
+    }
     return 0;
 }
 
